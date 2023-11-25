ac9c93778036ab0a934e673c6d8395af02fb68d3
Anirudh Gunde <anirudh.gunde1@gmail.com>
2023-11-25 15:13:00 -0600

    updated doc

---
+++ src/main/java/com/anirudhSpring/DMS/repository/DogRepository.java
@@ -0,0 +1,15 @@
+package com.anirudhSpring.DMS.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.anirudhSpring.DMS.Models.Dog;
+
+
+public interface DogRepository extends CrudRepository<Dog, Integer>{
+	List<Dog> findByName(String name);  
+	
+	
+
+}
---
+++ src/main/java/com/anirudhSpring/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.anirudhSpring.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.anirudhSpring.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
